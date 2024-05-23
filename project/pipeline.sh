89991e458d20a063c08a319a5fba450ea858ca85
farzan ghalami <ghalamif@gmail.com>
2024-05-23 13:59:03 +0200

    bug fix

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-python3 pipeline.py
+
+if command -v python3 &>/dev/null; then
+    python3 pipeline.py
+
+elif command -v python &>/dev/null; then
+    python pipeline.py
+
+else
+    echo "Python is not installed."
+    exit 1
+fi
